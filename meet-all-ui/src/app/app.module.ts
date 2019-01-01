78f418adf23ffae158bb181607f7c79e91605b8e
PINGUET Julien <pinguet62@gmail.com>
2019-01-01 16:04:57 +0100

    PWA

--- meet-all-ui/src/app/app.module.ts
+++ meet-all-ui/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
+import {ServiceWorkerModule} from '@angular/service-worker';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SecurityModule} from './security';
@@ -15,6 +17,7 @@ import {SecurityModule} from './security';
         BrowserModule,
         IonicModule.forRoot(),
         // app
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
         AppRoutingModule,
         SecurityModule.forRoot(),
     ],
