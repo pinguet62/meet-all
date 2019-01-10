08258ae1514247770bb97a52dc09e8f91f732a93
PINGUET Julien <pinguet62@gmail.com>
2019-01-10 23:13:39 +0100

    Fix Happn my_relation parsing
    fix 642bd5

--- meet-all-api/src/main/java/fr/pinguet62/meetall/provider/happn/dto/HappnRelation.java
+++ meet-all-api/src/main/java/fr/pinguet62/meetall/provider/happn/dto/HappnRelation.java
@@ -1,19 +1,36 @@
 package fr.pinguet62.meetall.provider.happn.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import static java.util.Arrays.stream;
 
 public enum HappnRelation {
 
     // new relation
-    @JsonProperty("0")
-    NEW_RELATION,
-
+    NEW_RELATION(0),
     // accepted by me but not (yet) by target
-    @JsonProperty("1")
-    ACCEPTED,
-
+    ACCEPTED(1),
     // matched
-    @JsonProperty("4")
-    MATCHED;
+    MATCHED(4);
+
+    private final int value;
+
+    HappnRelation(int value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static HappnRelation fromValue(int input) {
+        return stream(values())
+                .filter(it -> it.value == input)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown relation value " + input));
+    }
+
+    @JsonValue
+    public int toValue() {
+        return value;
+    }
 
 }
