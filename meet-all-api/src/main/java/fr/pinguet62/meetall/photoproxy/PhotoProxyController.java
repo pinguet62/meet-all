89424dd382189b3897bc6c73397fed1558c1fc10
PINGUET Julien <pinguet62@gmail.com>
2020-08-20 15:57:06 +0200

    fix(photoproxy): limit buffered data

--- meet-all-api/src/main/java/fr/pinguet62/meetall/photoproxy/PhotoProxyController.java
+++ meet-all-api/src/main/java/fr/pinguet62/meetall/photoproxy/PhotoProxyController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,17 +28,17 @@ class PhotoProxyController {
             summary = "Proxy for original image",
             responses = @ApiResponse(description = "The photo content", content = @Content(schema = @Schema(type = "string", format = "binary"))))
     @GetMapping("/photo/{encodedUrl:.+}")
-    public Mono<ResponseEntity<byte[]>> proxifyPhoto(@PathVariable @Parameter(example = "https%3A%2F%2Fwww.google.com%2Fimages%2Fbranding%2Fgooglelogo%2F1x%2Fgooglelogo_color_150x54dp.png", description = "Encoded original URL") String encodedUrl) {
+    public Mono<ResponseEntity<DataBuffer>> proxifyPhoto(@PathVariable @Parameter(example = "https%3A%2F%2Fwww.google.com%2Fimages%2Fbranding%2Fgooglelogo%2F1x%2Fgooglelogo_color_150x54dp.png", description = "Encoded original URL") String encodedUrl) {
         String originalUrl = PhotoProxyEncoder.decode(encodedUrl);
         return webClient
                 .get()
                 .uri(originalUrl)
                 .exchange()
-                .flatMap(res -> res.bodyToMono(byte[].class)
-                        .map(bytes -> ResponseEntity
+                .flatMap(res -> res.bodyToMono(DataBuffer.class)
+                        .map(body -> ResponseEntity
                                 .status(res.statusCode())
                                 .headers(res.headers().asHttpHeaders())
-                                .body(bytes)));
+                                .body(body)));
     }
 
 }
